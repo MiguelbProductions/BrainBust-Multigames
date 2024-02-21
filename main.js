f9bebe327c44e76aec35cd363b187b3a8f4aa575
Miguel Brandão <114339832+MiguelbProductions@users.noreply.github.com>
2024-02-21 17:02:30 +0000

    Login Working

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const path = require("path")
-const mongodb = require("mongodb")
+const { MongoClient } = require("mongodb");
 const session = require('express-session');
+const bcrypt = require('bcrypt');
 
 const app = express()
 
-const uri = 'mongodb://localhost:27017'
-const DB = "BrainBurst"
+const dbUrl = 'mongodb://localhost:27017';
+const dbName = "BrainBurst";
 
 app.engine("html", require("ejs").renderFile)
 app.set("view engine", "html")
@@ -25,13 +26,80 @@ app.use(session({
 }))
 
 app.get("/:page?", (req, res) => {
-  const page = req.params.page || "index";
+  const page = req.params.page || "Home";
   
-  res.render(`${page}.html`, {}, (err, html) => {
+  res.render(`${page}.html`, { currentPage: page }, (err, html) => {
+      if (err) { res.render("404.html", { currentPage: page });
+      } else { res.send(html); }
+  });
+});
+
+app.post('/login', async (req, res) => {
+  const { username_or_email, passwordvalue } = req.body;
+
+  try {
+      const client = await MongoClient.connect(dbUrl);
+      const db = client.db(dbName);
+      const users = db.collection("Users");
+
+      const user = await users.findOne({ $or: [{ username: username_or_email }, { email: username_or_email }] });
+
+      if (!user) {
+          res.render('login', { debug: {success: "", error: 'User not finded'}, currentPage: 'login' });
+          return;
+      }
+
+      if (passwordvalue !== user.password) {
+          res.render('login', { debug: {success: "", error: 'Incorrect Password'}, currentPage: 'login' });
+          return;
+      }
+
+      req.session.userId = user._id;
+      res.redirect('/');
+  } catch (err) {
+      console.error(err);
+      res.render('login', { debug: {success: "", error: 'Error acessing users'}, currentPage: 'login' });
+  }
+}).post('/register', async (req, res) => {
+  const { email, name, username, password_field, confirm_password } = req.body;
+
+    if(password_field !== confirm_password) {
+        return res.render('register', { debug: {success: "", error: 'Passwords do not match'}, currentPage: 'register' });
+    }
+
+    try {
+      const client = await MongoClient.connect(dbUrl, { useUnifiedTopology: true });
+      const db = client.db(dbName);
+      const users = db.collection("Users");
+
+      const existingUser = await users.findOne({ $or: [{ email }, { username }] });
+      if(existingUser) {
+          return res.render('register', { debug: {success: "", error: 'Email or username already exists'}, currentPage: 'register' });
+      }
+
+      const hashedPassword = await bcrypt.hash(password_field, 10);
+
+      await users.insertOne({
+          email,
+          name,
+          username,
+          password: hashedPassword, 
+      });
+      res.redirect('/login', { debug: {success: "User create sucefully", error: ''} , currentPage: 'login' });
+  } catch (err) {
+      console.error(err);
+      res.render('register', { debug: {success: "", error: 'Error accessing the database'} , currentPage: 'register' });
+  }
+});
+
+
+app.get('/logout', (req, res) => {
+  req.session.destroy(err => {
       if (err) {
-          res.render("404.html");
+          console.error(err);
+          res.send('Error');
       } else {
-          res.send(html);
+          res.redirect('..');
       }
   });
 });
